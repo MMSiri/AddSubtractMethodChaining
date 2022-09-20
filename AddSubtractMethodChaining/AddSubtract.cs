4fd97ad35779b4f91c1c9df96729264da046db31
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-09-20 19:31:51 +0100

    Add project files.

---
+++ AddSubtractMethodChaining/AddSubtract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddSubtractMethodChaining
+{
+    class AddSubtract
+    {
+        public int Value { get; set; }
+
+        public AddSubtract Add(int i)
+        {
+            Console.WriteLine($"Value: {Value}, adding {i}");
+            return new AddSubtract() { Value = Value + i };
+        }
+
+        public AddSubtract Subtract(int i)
+        {
+            Console.WriteLine($"Value: {Value}, subtracting {i}");
+            return new AddSubtract()
+            { Value = Value - i};
+        }
+    }
+}
---
+++ AddSubtractMethodChaining/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddSubtractMethodChaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddSubtract a = new AddSubtract() { Value = 5 }.Add(5).Subtract(3).Add(9).Subtract(12);
+            Console.WriteLine($"Result: {a.Value}");
+        }
+    }
+}
